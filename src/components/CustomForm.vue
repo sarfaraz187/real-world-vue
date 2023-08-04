a4296325d3933b6df7ef1e420a52e64099a34a6b
sarfaraz187 <sarfarazmohammed187@gmail.com>
2023-08-04 18:08:30 +0200

    new changes added

--- src/components/CustomForm.vue
+++ src/components/CustomForm.vue
@@ -1,27 +1,23 @@
 <template>
   <form @submit.prevent="handleSubmit">
     <h3>{{ title }}</h3>
-    <input v-model="email" type="email" /> <br />
-    <input v-model="password" type="password" /> <br />
+    <InputComponent v-model="email" type="email" v-bind:label="emailLabel" /> <br />
+    <InputComponent v-model="password" type="password" v-bind:label="passwordLabel" /> <br />
     <button>Submit</button>
   </form>
 </template>
 
-<script>
-console.log("Custom Form :");
+<script setup>
+import InputComponent from "./InputComponent.vue";
+import { ref } from "vue";
 
-export default {
-  data() {
-    return {
-      title: "Login Form",
-      email: "",
-      password: "",
-    };
-  },
-  methods: {
-    handleSubmit() {
-      console.log("Submitted :", this.email, this.password);
-    },
-  },
-};
+const emailLabel = ref("Email");
+const passwordLabel = ref("Password");
+
+function handleSubmit(event: any) {
+  // console.log("Submitted :", event, email, password);
+}
 </script>
+
+// export default { // data() { // return { // title: "Login Form", // email: "", // password: "", // emailLabel: "Email", // }; // }, // methods: { // handleSubmit() { // console.log("Submitted :",
+this.email, this.password); // }, // }, // };
---
+++ src/components/InputComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <label>
+    {{ props.label }} <br />
+    <input type="text" />
+  </label>
+</template>
+
+<script setup>
+const props = defineProps(["label"]);
+console.log(props);
+</script>
