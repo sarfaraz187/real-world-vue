8a00798713a5725a48088cd13d03b12e3534844a
sarfaraz187 <sarfarazmohammed187@gmail.com>
2023-08-04 23:17:32 +0200

    basic html and css added

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,123 @@
-<template></template>
+<template>
+  <main>
+    <!-- <div class="overlay">
+      <div class="modal">
+        <textarea name="Notes" id="notes" cols="30" rows="10"></textarea>
+        <button>Add Note</button>
+        <button class="close">Close</button>
+      </div>
+    </div> -->
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis magni in quis blanditiis nesciunt accusamus.</p>
+          <p class="date">04/08/2023</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
 
 <script></script>
+
+<style scoped>
+.main {
+  height: 100vh;
+  width: 100vw;
+}
+
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+
+header button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  border-radius: 90px;
+  color: white;
+  font-size: 20px;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(203, 231, 117);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+
+.date {
+  margin: 0px;
+  font-size: 12.5px;
+  font-weight: bold;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  width: 750px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+}
+
+.modal .close {
+  margin-top: 7px;
+  background-color: red;
+}
+</style>
